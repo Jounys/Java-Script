5a93d071290d6d8468b36385fca6b052f3d99354
João Paulo <103288654+Jounys@users.noreply.github.com>
2022-07-28 18:32:21 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ aula012/ex011.js
@@ -0,0 +1,13 @@
+var idade = 18
+console.log(`Você tem ${idade} anos`)
+if (idade < 16){
+    console.log('Não Vota')
+}else if(/*idade >= 16 && */idade < 18){
+        console.log('Voto opcional')
+    }else if(/*idade >= 18 && */idade < 65){
+            console.log('Voto Obrigatório')
+        }else{
+            console.log('Isento de Votar')
+        }
+
+// As funções comentadas já estão implicita sem ser necessaria
