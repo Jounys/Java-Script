0a1e471ce5018a413608f8adfd6d64c80065adf8
João Paulo <103288654+Jounys@users.noreply.github.com>
2022-08-19 17:44:58 -0300

    ha

---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,20 @@
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if (num.value.length == 0) {
+        window.alert('Por favor digite um número!')
+    } else {
+        let n = Number(num.value) 
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option') // Parte do HTML
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`                      // útil para outras linguagens
+            tab.appendChild(item)                       // Parte do HTML
+            c++
+        }
+    }
+    
+}
